ed287b8035e5cfa5d9d406f30ce6faf015313e94
Nagendra Srivastava <sanu.nagendra@gmail.com, nagendra@notikum.com>
2014-05-17 16:39:16 +0530

    code changes in demo app for notiphi

--- NotiphiSampleWithoutUserPermission/src/com/locuslabs/notiphisample/MainActivity.java
+++ NotiphiSampleWithoutUserPermission/src/com/locuslabs/notiphisample/MainActivity.java
@@ -1,10 +1,16 @@
 package com.locuslabs.notiphisample;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.notikum.notifypassive.NotiphiSession;
+import com.notikum.notifypassive.utils.NotiphiEventReceiver;
 
 public class MainActivity extends Activity {
 
@@ -12,7 +18,26 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		try{
+		Button button = (Button)findViewById(R.id.button_click);
+		
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				try{
+					JSONObject json = new JSONObject();
+					json.put("name", "Nagendra");
+					json.put("location", " BTM layout ");
+					new NotiphiEventReceiver(json, getApplicationContext());
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+				
+				
+			}
+		});
+		
+		try{ 
 			NotiphiSession.init(MainActivity.this,1);
 		}catch(Exception ex){
 			ex.printStackTrace();
