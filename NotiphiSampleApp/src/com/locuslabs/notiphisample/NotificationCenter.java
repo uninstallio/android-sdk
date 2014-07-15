29bb443b2d4f98e4da47a9401f53ebd26fb8be12
Nagendra Srivastava <sanu.nagendra@gmail.com, nagendra@notikum.com>
2014-07-15 12:12:18 +0530

    notification center implemented

--- NotiphiSampleApp/src/com/locuslabs/notiphisample/NotificationCenter.java
+++ NotiphiSampleApp/src/com/locuslabs/notiphisample/NotificationCenter.java
@@ -1,130 +1,130 @@
-package com.locuslabs.notiphisample;
-
-import java.util.ArrayList;
-
-import android.content.Context;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ListView;
-
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
-import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
-import com.locuslabs.notiphisample.fragment.NotiphiListFragment;
-import com.locuslabs.notiphisample.fragment.NotiphiMenuFragment;
-import com.notikum.notifypassive.utils.NotiphiPromotion;
-
-public class NotificationCenter extends SlidingFragmentActivity {
-
-	private final String TAG = NotificationCenter.class.getSimpleName();
-	private Context mContext;
-	private ListView mListView;
-	//private NotiphiListAdapter mNotiphiListAdapter;
-	private ArrayList<NotiphiPromotion> promotionList;
-	private boolean moreResult = false;
-	private Fragment mContent;
-	private Button mDetailsButton;
-	private Bundle savedInstanceState;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.notiphi_notification_center_main_layout);
-		mContext = this;
-		init();
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-	}
-	
-	public void switchContent(final Fragment fragment) {
-		mContent = fragment;
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.content_frame, fragment)
-		.commit();
-		Handler h = new Handler();
-		h.postDelayed(new Runnable() {
-			public void run() {
-				getSlidingMenu().showContent();
-			}
-		}, 50);
-	}
-	
-	private void init(){
-
-		setContentView(R.layout.responsive_content_frame);
-		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		mDetailsButton = (Button)findViewById(R.id.details_button);
-		mDetailsButton.setVisibility(View.VISIBLE);
-		// check if the content frame contains the menu frame
-		if (findViewById(R.id.menu_frame) == null) {
-			Log.d(TAG, "Menu frame is null");
-			setBehindContentView(R.layout.menu_frame);
-			getSlidingMenu().setSlidingEnabled(true);
-			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-			
-		} else {
-			// add a dummy view
-			Log.d(TAG, "Menu frame is  not null");
-			View v = new View(this);
-			setBehindContentView(v);
-			getSlidingMenu().setSlidingEnabled(false);
-			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-		}
-		
-		// set the Above View Fragment
-		if (savedInstanceState != null)
-			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
-		if (mContent == null)
-			mContent = new NotiphiListFragment(mContext,0);	 // default All notification
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.content_frame, mContent)
-		.commit();
-		// set the Behind View Fragment
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.menu_frame, new NotiphiMenuFragment(mContext))
-		.commit();	
-		// customize the SlidingMenu
-		SlidingMenu sm = getSlidingMenu();
-		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		sm.setShadowWidthRes(R.dimen.shadow_width);
-		sm.setShadowDrawable(R.drawable.shadow);
-		sm.setBehindScrollScale(0.25f);
-		sm.setFadeDegree(0.25f);
-		
-		mDetailsButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				toggle();
-			}
-		});
-	
-	
-	}
-
-}
+//package com.locuslabs.notiphisample;
+//
+//import java.util.ArrayList;
+//
+//import android.content.Context;
+//import android.os.Bundle;
+//import android.os.Handler;
+//import android.support.v4.app.Fragment;
+//import android.util.Log;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.widget.Button;
+//import android.widget.ListView;
+//
+//import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+//import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+//import com.locuslabs.notiphisample.fragment.NotiphiListFragment;
+//import com.locuslabs.notiphisample.fragment.NotiphiMenuFragment;
+//import com.notikum.notifypassive.utils.NotiphiPromotion;
+//
+//public class NotificationCenter extends SlidingFragmentActivity {
+//
+//	private final String TAG = NotificationCenter.class.getSimpleName();
+//	private Context mContext;
+//	private ListView mListView;
+//	//private NotiphiListAdapter mNotiphiListAdapter;
+//	private ArrayList<NotiphiPromotion> promotionList;
+//	private boolean moreResult = false;
+//	private Fragment mContent;
+//	private Button mDetailsButton;
+//	private Bundle savedInstanceState;
+//
+//	@Override
+//	public void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.notiphi_notification_center_main_layout);
+//		mContext = this;
+//		init();
+//	}
+//
+//	@Override
+//	public void onStart() {
+//		super.onStart();
+//	}
+//
+//	@Override
+//	public void onResume() {
+//		super.onResume();
+//	}
+//
+//	@Override
+//	public void onPause() {
+//		super.onPause();
+//	}
+//
+//	@Override
+//	public void onDestroy() {
+//		super.onDestroy();
+//	}
+//	
+//	public void switchContent(final Fragment fragment) {
+//		mContent = fragment;
+//		getSupportFragmentManager()
+//		.beginTransaction()
+//		.replace(R.id.content_frame, fragment)
+//		.commit();
+//		Handler h = new Handler();
+//		h.postDelayed(new Runnable() {
+//			public void run() {
+//				getSlidingMenu().showContent();
+//			}
+//		}, 50);
+//	}
+//	
+//	private void init(){
+//
+//		setContentView(R.layout.responsive_content_frame);
+//		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//		mDetailsButton = (Button)findViewById(R.id.details_button);
+//		mDetailsButton.setVisibility(View.VISIBLE);
+//		// check if the content frame contains the menu frame
+//		if (findViewById(R.id.menu_frame) == null) {
+//			Log.d(TAG, "Menu frame is null");
+//			setBehindContentView(R.layout.menu_frame);
+//			getSlidingMenu().setSlidingEnabled(true);
+//			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+//			
+//		} else {
+//			// add a dummy view
+//			Log.d(TAG, "Menu frame is  not null");
+//			View v = new View(this);
+//			setBehindContentView(v);
+//			getSlidingMenu().setSlidingEnabled(false);
+//			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+//		}
+//		
+//		// set the Above View Fragment
+//		if (savedInstanceState != null)
+//			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
+//		if (mContent == null)
+//			mContent = new NotiphiListFragment(mContext,0);	 // default All notification
+//		getSupportFragmentManager()
+//		.beginTransaction()
+//		.replace(R.id.content_frame, mContent)
+//		.commit();
+//		// set the Behind View Fragment
+//		getSupportFragmentManager()
+//		.beginTransaction()
+//		.replace(R.id.menu_frame, new NotiphiMenuFragment(mContext))
+//		.commit();	
+//		// customize the SlidingMenu
+//		SlidingMenu sm = getSlidingMenu();
+//		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+//		sm.setShadowWidthRes(R.dimen.shadow_width);
+//		sm.setShadowDrawable(R.drawable.shadow);
+//		sm.setBehindScrollScale(0.25f);
+//		sm.setFadeDegree(0.25f);
+//		
+//		mDetailsButton.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				toggle();
+//			}
+//		});
+//	
+//	
+//	}
+//
+//}
